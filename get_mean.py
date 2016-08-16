167417f054e7a624674a8a283b601e583339246f
Eric Bigelow <ebigelow@u.rochester.edu>
2016-08-16 16:43:27 -0400

    Added instructions, os import.

--- get_mean.py
+++ get_mean.py
@@ -1,10 +1,17 @@
-import cv2
-"""
 
-TODO: instructions . . .
+"""
+Use this file to extract the mean of a directory of images.
+
+With CNN we subract mean from all images from each image, so on 
+average each channel has values centered around 0.
+
+Example:
+  $ python get_mean.py -i /path/to/images -o /path/to/output/mean.npy
 
 """
 import numpy as np
+import cv2
+import os
 from optparse import OptionParser
 
 if __name__ == '__main__':
@@ -13,8 +20,8 @@ if __name__ == '__main__':
     parser.add_option("-i", "--in", dest="img_dir",
                       type="string", default="./images",
                       help="Images directory.")
-    parser.add_option("-o", "--out", dest="out_dir",
-                      type="string", default="./model",
+    parser.add_option("-o", "--out", dest="out_file",
+                      type="string", default="./model/mean.npy",
                       help="Out directory.")
     (options, args) = parser.parse_args()
     img_dir = options.img_dir
@@ -23,8 +30,8 @@ if __name__ == '__main__':
 
     Mu = np.zeros(3)
     for img_file in img_files:
-        img = cv2.imread(img_dir + '/' + img_file).astype(float)
+        img = cv2.imread(os.path.join(img_dir,img_file)).astype(float)
         Mu += np.mean(img, axis=(0,1)) / N
 
-    np.save(options.out_dir + 'mean.npy', Mu)
+    np.save(options.out_file, Mu)
 
